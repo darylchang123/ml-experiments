9f5466f97ea1fdfb956e8735e7a3ab457f7e4715
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-09 00:44:34 +0000

    Adding util file and a sample notebook to perform end to end training

---
+++ utils/ml_utils.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import urllib3
+urllib3.disable_warnings()
+
+SHUFFLE_SEED = 524287
+
+
+def load_dataset(dataset_name, shuffle_seed=SHUFFLE_SEED):
+    """
+    Loads the tensorflow datasets
+    :param dataset_name: One of the following dataset names: https://www.tensorflow.org/datasets/catalog/overview
+    :param shuffle_seed: Seed for shuffling
+    :return: (raw_train, raw_validation, raw_test), label_metadata
+    """
+    read_config = tfds.ReadConfig(shuffle_seed=shuffle_seed)
+    if dataset_name == 'cats_vs_dogs':
+        (raw_train, raw_validation, raw_test), metadata = tfds.load(
+            'cats_vs_dogs',
+            split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+            shuffle_files=True,
+            with_info=True,
+            as_supervised=True,
+            read_config=read_config
+        )
+        print('Training Data Summary')
+        summarize_dataset(raw_train)
+        print('\nValidation Data Summary')
+        summarize_dataset(raw_validation)
+        print('\nTest Data Summary')
+        summarize_dataset(raw_test)
+        return (raw_train, raw_validation, raw_test), metadata.features['label'].names
+    return None
+
+
+def summarize_dataset(tf_data):
+    """
+    Prints stats around no. of classes in the dataset
+    :param tf_data: PrefetchDataset
+    """
+    label = np.array([l for _, l in tf_data])
+    class_freq = np.array(np.unique(label, return_counts=True)).transpose()
+    class_summary = {f[0]: (f[1], f[1] * 100 / len(label)) for f in class_freq}
+    print('No. of examples: {count}'.format(count=len(label)))
+    for k,v in class_summary.items():
+        print('Class: {class_val} :::: Count: {count} :::: Percentage: {percent}'.format(
+            class_val=k,
+            count=v[0],
+            percent=v[1]
+        ))
+
+
+def resize_image(image, label, img_size):
+    """
+    Resizes image
+    :param image: Image
+    :param label: Label
+    :param img_size: Image size
+    :return:
+    """
+    image = tf.cast(image, tf.float32)
+    image = image/255
+    image = tf.image.resize(image, (img_size, img_size))
+    return image, label
+
+
+def show_image(image, label, label_names):
+    """
+    Shows image
+    :param image: Image
+    :param label: Label
+    :param label_names: List containing label names
+    :return:
+    """
+    plt.figure()
+    plt.imshow(image)
+    plt.title('Class: {class_value} :::: Class Name: {label_name}'.format(
+        class_value=label,
+        label_name=label_names[label] if len(label_names)>label else ''
+    ))
+
+
+def summarize_diagnostics(history):
+    """
+    # Plot diagnostic learning curves
+    :param history: Keras history object
+    :return: Show plots
+    """
+    # Plot loss
+    plt.figure(figsize=(10, 10), dpi=80)
+    plt.subplot(211)
+    plt.title('Cross Entropy Loss')
+    plt.plot(history.history['loss'], color='blue', label='train')
+    plt.plot(history.history['val_loss'], color='orange', label='val')
+    plt.xlabel('Epoch')
+    plt.ylabel('Cross Entropy Loss')
+    plt.grid(True)
+    plt.legend()
+    # Plot accuracy
+    plt.subplot(212)
+    plt.title('Classification Accuracy')
+    plt.plot(history.history['accuracy'], color='blue', label='train')
+    plt.plot(history.history['val_accuracy'], color='orange', label='val')
+    plt.xlabel('Epoch')
+    plt.ylabel('Classification Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
